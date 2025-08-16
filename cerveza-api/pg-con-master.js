7435d515a1624279f149dc99135d0fee29067272
Iván Amaya <amaya.ivan333@gmail.com>
2025-08-16 15:59:22 -0400

    Conexion a PostgreSQL en Render

--- cerveza-api/pg-con-master.js
+++ cerveza-api/pg-con-master.js
@@ -1,8 +1,16 @@
 const pgp = require("pg-promise")();
-require("dotenv").config();
 
-const connection = process.env.DATABASE_URL;
+// Si existe DATABASE_URL (Render), usarla; si no, fallback a local
+const connection = process.env.DATABASE_URL || {
+  host: "localhost",
+  port: 5432,
+  database: "postgres",
+  user: "postgres",
+  password: "admin",
+};
 
 const db = pgp(connection);
+
+console.log("✅ pg-promise listo para usar");
 
 module.exports = db;
